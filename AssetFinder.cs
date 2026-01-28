3f977b0a3163ee7c2d68659093d40d42d2399c96
FantasyGarden <54232409+chesherneko@users.noreply.github.com>
2026-01-28 22:15:49 +0900

    Add files via upload

---
+++ AssetFinder.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class AssetFinder
+{
+    public static bool TryGetAsset<T>(string folderPath, out T result) where T : Object
+    {
+        result = default;
+
+        var filter = $"t:{typeof(T).Name}";
+        var guids = AssetDatabase.FindAssets(filter, new[] { folderPath });
+
+        if (guids.Length <= 0)
+        {
+            Debug.LogWarning($"No asset of type {typeof(T).Name} was found.");
+            return false;
+        }
+
+        var filePath = AssetDatabase.GUIDToAssetPath(guids[0]);
+        result = AssetDatabase.LoadAssetAtPath<T>(filePath);
+
+        return true;
+    }
+    
+    public static bool TryGetAsset<TSearch, TResult>(string folderPath, out TResult result) 
+        where TSearch : TResult
+        where TResult : Object
+    {
+        result = default;
+
+        if (!TryGetAsset(folderPath, out TSearch rawResult))
+            return false;
+
+        if (rawResult is not TResult casted)
+        {
+            Debug.LogWarning(
+                $"Asset type mismatch in '{folderPath}'. " +
+                $"Expected {typeof(TResult).Name}, but got {rawResult.GetType().Name}.");
+            return false;
+        }
+
+        result = casted;
+        return true;
+    }
+}
